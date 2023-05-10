7fc5aae397b38c0aaf304da462822a5a1eafffd3
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-05-10 21:08:10 +0200

    Update queue.h

--- queue.h
+++ queue.h
@@ -8,15 +8,18 @@ struct element {
 };
 
 typedef struct queue {
-	// Define the struct yourself
-	int param1;
+	int head; //index for the first element in the queue
+    int tail; //index for the last element in the queue
+    int size; // fixed amount of data capable of storing
+    int *data; // actual array of length size of data to store
 }queue;
 
 queue* queue_init (int size);
-int queue_destroy (queue *q);
-int queue_put (queue *q, struct element* elem);
-struct element * queue_get(queue *q);
 int queue_empty (queue *q);
 int queue_full(queue *q);
+int queue_put (queue *q, struct element* elem);
+struct element * queue_get(queue *q);
+int queue_destroy (queue *q);
 
 #endif
+
