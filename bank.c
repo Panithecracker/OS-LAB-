23483c508d19e4b0c08614fac9e3c51963ae0663
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-05-10 20:49:21 +0200

    Add files via upload

---
+++ bank.c
@@ -0,0 +1,26 @@
+//OS-P3 2022-2023
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include "queue.h"
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+/**
+ * Entry point
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main (int argc, const char * argv[] ) {
+    printf("Hello world!");
+    return 0;
+}
