b979d6788fe6bd44a68aef2ce93dda7b0b1f83a5
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-05-11 23:00:47 +0200

    Update queue.c
    Really close!

--- queue.c
+++ queue.c
@@ -81,3 +81,91 @@ int queue_destroy(queue *cq){
 	return 0;
 }
 
+//function that does the reading and storing info inside array of elements (it modifies the input element array + returns the first line integer on the file referring to the number of operations )
+int read_operations(char *file, struct element *list_client_ops)
+{
+    FILE *fp = fopen(file, "r");
+	    if (fp == NULL) {
+		perror("Error opening file");
+		return -1;
+	    }
+	    
+	    char line[100];
+		int numops = 0; 
+		fgets(line, 100, fp);
+		numops = atoi(line);
+	    int i = 0;
+	    while (fgets(line, 100, fp) != NULL) {
+	    	// Initialize amount to 0 for each new element
+        	list_client_ops[i].amount = 0.0;
+	    
+		char *token = strtok(line, " ");
+		if (strcmp(token, "CREATE") == 0) {
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].operation_id = 1;
+		    list_client_ops[i].account_number = atoi(token);
+		} else if (strcmp(token, "DEPOSIT") == 0) {
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].operation_id = 2;
+		    list_client_ops[i].account_number = atoi(token);
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].amount = atof(token);
+		} else if (strcmp(token, "TRANSFER") == 0) {
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].operation_id = 3;
+		    list_client_ops[i].acc_from = atoi(token);
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].acc_to = atoi(token);
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].amount = atof(token);
+		} else if (strcmp(token, "WITHDRAW") == 0) {
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].operation_id = 4;
+		    list_client_ops[i].account_number = atoi(token);
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].amount = atof(token);
+		} else if (strcmp(token, "BALANCE") == 0) {
+		    token = strtok(NULL, " ");
+		    list_client_ops[i].operation_id = 5;
+		    list_client_ops[i].account_number = atoi(token);
+		}
+		i++;
+	    }
+    
+	    fclose(fp);
+        return numops;
+}
+
+
+//function that performs an ATM request given an operation ie : element object, which potentially accesses balance array and global balance variable
+int take_action(struct element *data, int *balance, int global_balance, int bank_numop)
+{
+		switch (data->operation_id) {
+		    case 1: // CREATE
+		      balance[data->account_number] = 0;
+		      printf("%d CREATE %d BALANCE=%d TOTAL=%d\n", bank_numop+1, data->account_number, balance[data->account_number], global_balance);
+		      return 0;
+		    case 2: // DEPOSIT
+		      balance[data->account_number] += data->amount;
+		      global_balance += data->amount;
+		      printf("%d DEPOSIT %d %d BALANCE=%d TOTAL=%d\n", bank_numop+1, data->account_number, data->amount, balance[data->account_number], global_balance);
+			  return 0;
+		    case 3: // TRANSFER
+		      // Total stays the same (Balance of the accounts change)
+		      balance[data->acc_from] -= data->amount;
+		      balance[data->acc_to] += data->amount;
+		      printf("%d TRANSFER %d %d %d BALANCE=%d TOTAL=%d\n", bank_numop+1, data->acc_from, data->acc_to, data->amount, balance[data->acc_to], global_balance);
+			  return 0;
+		    case 4: // WITHDRAW
+		      balance[data->account_number] -= data->amount;
+		      global_balance -= data->amount;
+		      printf("%d WITHDRAW %d %d BALANCE=%d TOTAL=%d\n", bank_numop+1, data->account_number, data->amount, balance[data->account_number], global_balance);
+		   	  return 0;
+		    case 5: // BALANCE
+		      // Nada creo
+		      printf("%d BALANCE %d BALANCE=%d TOTAL=%d\n", bank_numop+1, data->account_number, balance[data->account_number], global_balance);
+		      return 0;
+		    default:
+		      perror("Not valid operation");
+		}
+}
